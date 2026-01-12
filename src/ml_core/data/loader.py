4d6ce9c191f2ce09727183e21e1335e840e01577
Hugovveen <hugovanveen4@gmail.com>
2026-01-12 17:53:17 +0100

    added loader and file to gitignore

--- src/ml_core/data/loader.py
+++ src/ml_core/data/loader.py
@@ -15,16 +15,62 @@ def get_dataloaders(config: Dict) -> Tuple[DataLoader, DataLoader]:
     data_cfg = config["data"]
     base_path = Path(data_cfg["data_path"])
 
-    # TODO: Define Transforms
-    # train_transform = ...
-    # val_transform = ...
+    # -----------------------
+    # Transforms
+    # -----------------------
+    train_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+        ]
+    )
 
-    # TODO: Define Paths for X and Y (train and val)
-    
-    # TODO: Instantiate PCAMDataset for train and val
+    val_transform = transforms.Compose(
+        [
+            transforms.ToTensor(),
+        ]
+    )
 
-    # TODO: Create DataLoaders
-    # train_loader = ...
-    # val_loader = ...
-    
-    raise NotImplementedError("Implement get_dataloaders")
+    # -----------------------
+    # Paths
+    # -----------------------
+    x_train = base_path / "x_train.h5"
+    y_train = base_path / "y_train.h5"
+
+    x_val = base_path / "x_val.h5"
+    y_val = base_path / "y_val.h5"
+
+    # -----------------------
+    # Datasets
+    # -----------------------
+    train_dataset = PCAMDataset(
+        x_path=str(x_train),
+        y_path=str(y_train),
+        transform=train_transform,
+    )
+
+    val_dataset = PCAMDataset(
+        x_path=str(x_val),
+        y_path=str(y_val),
+        transform=val_transform,
+    )
+
+    # -----------------------
+    # DataLoaders
+    # -----------------------
+    train_loader = DataLoader(
+        train_dataset,
+        batch_size=data_cfg["batch_size"],
+        shuffle=True,
+        num_workers=data_cfg.get("num_workers", 4),
+        pin_memory=True,
+    )
+
+    val_loader = DataLoader(
+        val_dataset,
+        batch_size=data_cfg["batch_size"],
+        shuffle=False,
+        num_workers=data_cfg.get("num_workers", 4),
+        pin_memory=True,
+    )
+
+    return train_loader, val_loader
