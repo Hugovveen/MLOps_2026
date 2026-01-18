03b34bacb26a90cb5f4776d688766ef11e11b5fe
Davidvangoethem <david.van.goethem@student.uva.nl>
2026-01-18 14:58:23 +0100

    implement and use seed_everything

--- experiments/train.py
+++ experiments/train.py
@@ -1,10 +1,11 @@
+from ml_core.utils.logging import seed_everything
 from ml_core.utils import load_yaml_config
 import argparse
 import torch
 import torch.optim as optim
-# from ml_core.data import get_dataloaders
-# from ml_core.models import MLP
-# from ml_core.solver import Trainer
+from ml_core.data import get_dataloaders
+from ml_core.models import MLP
+from ml_core.solver import Trainer
 # from ml_core.utils import load_config, seed_everything, setup_logger
 
 # logger = setup_logger("Experiment_Runner")
@@ -12,12 +13,28 @@ import torch.optim as optim
 def main(args):
     # 1. Load Config & Set Seed
     config = load_yaml_config(args.config)
+    seed = int(config.get("seed", 42))
+    seed_everything(seed)
+    print(f"Using seed: {seed}")
+ 
+    device = "cuda" if torch.cuda.is_available() else "cpu"
+    print (f"using device: {device}")
 
-    print("Loaded config:", args.config)
-    print("seed:", config.get("seed"))
-    print("data.batch_size:", config["data"]["batch_size"])
-    print("training.epochs:", config["training"]["epochs"])
-    print("training.learning_rate:", config["training"]["learning_rate"])
+    train_loader, val_loader = get_dataloaders(config)
+
+    model = MLP(**config["model"])
+
+    optimizer = optim.SGD(model.parameters(),
+        lr = config['training']['learning_rate'],)
+
+    trainer = Trainer(
+        model=model,
+        optimizer=optimizer,
+        config=config,
+        device=device,
+    )
+
+    trainer.fit(train_loader, val_loader)
     
     # 2. Setup Device
     
