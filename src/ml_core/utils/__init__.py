daa16c249df990b2ee0937e220cd9fa7d21a480b
Hugovveen <hugovanveen4@gmail.com>
2026-01-21 17:01:20 +0100

    fixed init

--- src/ml_core/utils/__init__.py
+++ src/ml_core/utils/__init__.py
@@ -1,5 +1,10 @@
-from .config import load_yaml_config
+from .config import load_yaml_config as load_config
 from .logging import seed_everything, setup_logger
 from .tracker import ExperimentTracker
 
-__all__ = ["setup_logger", "seed_everything", "load_yaml_config", "ExperimentTracker"]
+__all__ = [
+    "setup_logger",
+    "seed_everything",
+    "load_config",
+    "ExperimentTracker",
+]
